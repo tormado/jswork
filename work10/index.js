feaee2f7d64bcc5fbb4928ca94c124e45eff6cda
tormado <2391816986@qq.com>
2019-10-31 20:02:26 +0800

    work10

---
+++ work10/index.js
@@ -0,0 +1,43 @@
+function monkey(){
+    let total = document.getElementById("monkeytotal").value
+    let kick = document.getElementById("monkeykick").value
+    total = parseInt(total) && Number(total)
+    kick = parseInt(kick) && Number(kick)
+    if(isNaN(total) || isNaN(kick)){
+        alert('请输入数字')
+        return
+    }
+    let monkey=[]
+    for (let i = 1 ;i<=total;i++){
+        monkey.push(i)
+    }
+    let i=0
+    while(monkey.length>1){
+        i++;
+        head = monkey.shift()
+        if(i%kick!=0){
+        monkey.push(head);
+        }
+    }
+    document.getElementById('monkeyking').innerText = monkey[0]
+}
+
+function stat(){
+    let str = document.getElementById("str").value
+    let obj = {}
+    // document.write(str.split("")+"<br>")直接用会清空上面的代码
+        for(let i=0;i<str.length;i++) {
+       var key=str[i];//str.charAt(i);
+        if(obj[key]){
+            obj[key]++;
+        }else{
+            obj[key] = 1;
+        }
+        // if(!obj[key]){
+        // obj[key] = 1;
+        // }else{
+        //     obj[key]++;
+        // }
+    }
+    document.getElementById('result').innerText = JSON.stringify(obj)
+}
--- work8/goldbach.js
+++ work8/goldbach.js
@@ -1,10 +1,11 @@
 function goldbach(){let str=''
-var z=document.getElementById('odd').value
-var x=1,w=0;var t=0;var aaa=[];
-if(z%2==0){
-for(var i=2;i<z;i++){ qqq=true;
-    for(var x=2;x<i;x++){  if( i%x==0 ){qqq=false;break;}   }
-    if(qqq){   aaa[w]=i;  w++;
+    var z=document.getElementById('odd').value
+    var x=1,w=0;var t=0;var aaa=[];
+    if(z%2==0){
+        for(var i=2;i<z;i++){
+        qqq=true;
+        for(var x=2;x<i;x++){  if( i%x==0 ){qqq=false;break;}   }
+        if(qqq){   aaa[w]=i;  w++;
         
     }  
 }
